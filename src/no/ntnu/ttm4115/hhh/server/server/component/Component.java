a7eac71a4a1c4a44e10173dee15325b99936355b
burkongla <stianmichael@gmail.com>
2015-05-05 09:31:19 +0200

    Working like a charm!

--- src/no/ntnu/ttm4115/hhh/server/server/component/Component.java
+++ src/no/ntnu/ttm4115/hhh/server/server/component/Component.java
@@ -11,38 +11,81 @@ public class Component extends Block {
 	public float CurrentTemperature;
 	public float DesiredTemperature;
 	public java.lang.String Status;
-
+	public java.lang.String newTopic;
+	public java.lang.String looPayload;
+	
 	public Parameters MQTTSetup() {
+		setLooPayload("");
 		MQTTConfigParam m = new MQTTConfigParam("dev.bitreactive.com");
-		m.addSubscribeTopic("hhh/termostats");
-		m.addSubscribeTopic("hhh/mobile");
+		m.addSubscribeTopic("hhh/server");
 		Parameters p = new Parameters(m);
 		return p;
 	}
 
-	public boolean msgHandler(MQTTMessage m) {
-		if (m.getTopic().equals("hhh/termostats")) {
-			String[] message = new String(m.getPayload()).split("\\s+");
-			if (message[0].equals("Current:")) {
-				this.CurrentTemperature = Float.valueOf(message[1]);
-				return false;
-			} else if (message[0].equals("Desired:")) {
-				this.DesiredTemperature = Float.valueOf(message[1]);
-				return true;
-			}
-		} else {
-			return false;
+	public void msgHandler(MQTTMessage m) {
+		String[] message = new String(m.getPayload()).split("\\s+");
+		if (message[0].equals("Current:")) {
+			this.CurrentTemperature = Float.valueOf(message[1]);
+			setLooPayload("Current: " + message[1]);
+			System.out.println("Current: " + String.valueOf(this.CurrentTemperature));
+			setNewTopic("hhh/mobile");
+		} else if (message[0].equals("Desired:")) {
+			this.DesiredTemperature = Float.valueOf(message[1]);
+			setLooPayload("Desired: " + message[1]);
+			System.out.println("Desired: " + String.valueOf(this.DesiredTemperature));
+			setNewTopic("hhh/thermostats");
 		}
-		return false;
 	}
 
 	public MQTTMessage createMessage() {
-		String payload = "Desired:"+" "+String.valueOf(DesiredTemperature);
+		String payload = getLooPayload();
+		System.out.println("Payload: " + payload);
 		byte[] bytes = payload.getBytes();
-	    String topic = "hhh/termostats";
+	    String topic = getNewTopic();
+	    System.out.println("Topic: " + topic);
 		MQTTMessage message = new MQTTMessage(bytes, topic);
 		message.setQoS(2);
 		return message;
 	}
 
+	public float getCurrentTemperature() {
+		return CurrentTemperature;
+	}
+
+	public void setCurrentTemperature(float currentTemperature) {
+		CurrentTemperature = currentTemperature;
+	}
+
+	public float getDesiredTemperature() {
+		return DesiredTemperature;
+	}
+
+	public void setDesiredTemperature(float desiredTemperature) {
+		DesiredTemperature = desiredTemperature;
+	}
+
+	public java.lang.String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(java.lang.String status) {
+		Status = status;
+	}
+
+	public java.lang.String getNewTopic() {
+		return newTopic;
+	}
+
+	public void setNewTopic(java.lang.String newTopic) {
+		this.newTopic = newTopic;
+	}
+
+	public java.lang.String getLooPayload() {
+		return looPayload;
+	}
+
+	public void setLooPayload(java.lang.String looPayload) {
+		this.looPayload = looPayload;
+	}
+	
 }
